46200f0c5a96c9d3f5d11008684e52ec20086c6a
putsomething <72783906+putsomething@users.noreply.github.com>
2020-10-13 14:52:10 +0800

    Add files via upload

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,16 @@
+// ConsoleApplication1.cpp : 定义控制台应用程序的入口点。
+//
+
+#include "stdafx.h"
+#include "stdio.h"
+#include <windows.h>
+
+int main(int argc, _TCHAR* argv[])
+{
+	float a = 12300/ 1000;
+	printf("%f", a);
+	Sleep(1000);
+	system("pause");
+	return 0;
+}
+
